8b7d537919753d6f422bc5acaa5a2f4aed94bd78
eddy <eduardo8@pdx.edu>
2023-11-16 21:09:12 -0800

    moved struct definition
    moved MemoryRequest_t to common.h

--- include/memory_request.h
+++ include/memory_request.h
@@ -10,20 +10,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
-typedef struct MemoryRequest {
-  unsigned long time;
-  uint8_t core;
-  uint8_t operation;
-  // unsigned long long address;
-  uint8_t byte_select;  // 2 bits
-  uint8_t column_low;   // 4 bits (column[3:0])
-  uint8_t channel;      // 1 bit
-  uint8_t bank_group;   // 3 bits
-  uint8_t bank;         // 2 bits
-  uint8_t column_high;  // 6 bits (column[9:4])
-  uint32_t row;         // 16 bits
-} MemoryRequest_t;
+#include "common.h"
 
 void memory_request_init(MemoryRequest_t *memoryRequest, unsigned long time,
                          uint8_t core, uint8_t operation,
