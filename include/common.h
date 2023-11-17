11f83dbdb84694a8d14e8831c19a8d4807832301
eddy <eduardo8@pdx.edu>
2023-11-16 21:06:18 -0800

    moved MemoryRequest_t struct here
    this structed is used in both the linked list and queue api and i didnt want to redefine it in multple places. Although, it does feel out of place here...

--- include/common.h
+++ include/common.h
@@ -1,6 +1,12 @@
 #ifndef __COMMON_H__
 #define __COMMON_H__
 
+/*** includes ***/
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdint.h>
+
+/*** macro(s) ***/
 #ifdef DEBUG
 #define LOG_DEBUG(format, ...) printf("%s:%d: " format, __FILE__, __LINE__, ##__VA_ARGS__)
 #define LOG(format, ...) printf(format, ##__VA_ARGS__)
@@ -8,5 +14,20 @@
 #define LOG_DEBUG(...) /*** expands to nothing ***/
 #define LOG(...)       /*** expands to nothing ***/
 #endif
+
+/*** struct(s) ***/
+typedef struct MemoryRequest {
+  unsigned long time;
+  uint8_t core;
+  uint8_t operation;
+  // unsigned long long address;
+  uint8_t byte_select;  // 2 bits
+  uint8_t column_low;   // 4 bits (column[3:0])
+  uint8_t channel;      // 1 bit
+  uint8_t bank_group;   // 3 bits
+  uint8_t bank;         // 2 bits
+  uint8_t column_high;  // 6 bits (column[9:4])
+  uint32_t row;         // 16 bits
+} MemoryRequest_t;
 
 #endif
